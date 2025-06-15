ab5f701555bb379ecc5bbcc5e97f7fb616de7671
Bibek4998 <bibekjoshi07@gmail.com>
2025-06-15 08:29:10 +0545

    Encryption and Decryption in RSA

---
+++ RSAExample.java
@@ -0,0 +1,31 @@
+import java.security.*;
+import javax.crypto.Cipher;
+import java.util.Base64;
+import java.util.Scanner;
+public class RSAExample {
+    public static void main(String[] args) throws Exception {
+
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+        keyGen.initialize(2048);
+        KeyPair keyPair = keyGen.generateKeyPair();
+        PublicKey publicKey = keyPair.getPublic();
+        PrivateKey privateKey = keyPair.getPrivate();
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter text to encrypt: ");
+        String message = sc.nextLine();
+
+        Cipher encryptCipher = Cipher.getInstance("RSA");
+        encryptCipher.init(Cipher.ENCRYPT_MODE, publicKey);
+        byte[] encryptedBytes = encryptCipher.doFinal(message.getBytes());
+        String encryptedText = Base64.getEncoder().encodeToString(encryptedBytes);
+        System.out.println("Encrypted: " + encryptedText);
+
+        Cipher decryptCipher = Cipher.getInstance("RSA");
+        decryptCipher.init(Cipher.DECRYPT_MODE, privateKey);
+        byte[] decryptedBytes = decryptCipher.doFinal(Base64.getDecoder().decode(encryptedText));
+        String decryptedText = new String(decryptedBytes);
+        System.out.println("Decrypted: " + decryptedText);
+        sc.close();
+    }
+}
