84822ad4ae90b6fd7d8cc5046002bdfa62f1fdcc
Bibek4998 <bibekjoshi07@gmail.com>
2025-06-15 08:13:53 +0545

    Cleared some spacing in the code and closed the sc

--- CaesarEncryption.java
+++ CaesarEncryption.java
@@ -1,11 +1,9 @@
 // Write a java program to implement the caesar cipher encryption  in Java.
 import java.util.Scanner;
-
 public class CaesarEncryption {
     public static String encrypt(String text, int shift) {
         StringBuilder result = new StringBuilder();
         shift = shift % 26;
-
         for (char c : text.toCharArray()) {
             if (Character.isUpperCase(c)) {
                 char ch = (char)(((c - 'A' + shift) % 26) + 'A');
@@ -14,21 +12,19 @@ public class CaesarEncryption {
                 char ch = (char)(((c - 'a' + shift) % 26) + 'a');
                 result.append(ch);
             } else {
-                result.append(c); // non-alphabetic characters remain unchanged
+                result.append(c);
             }
         }
-
         return result.toString();
     }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter plaintext: ");
         String text = sc.nextLine();
         System.out.println("Enter shift key: ");
         int shift = sc.nextInt();
-
         String encrypted = encrypt(text, shift);
         System.out.println("Encrypted text: " + encrypted);
+        sc.close();
     }
 }
