f90e70e56a3185a791c0afc7ae9506d6bbef0645
Bibek4998 <bibekjoshi07@gmail.com>
2025-06-15 08:09:45 +0545

    Added the caesar cipher encryption in java

---
+++ CaesarEncryption.java
@@ -0,0 +1,34 @@
+// Write a java program to implement the caesar cipher encryption  in Java.
+import java.util.Scanner;
+
+public class CaesarEncryption {
+    public static String encrypt(String text, int shift) {
+        StringBuilder result = new StringBuilder();
+        shift = shift % 26;
+
+        for (char c : text.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                char ch = (char)(((c - 'A' + shift) % 26) + 'A');
+                result.append(ch);
+            } else if (Character.isLowerCase(c)) {
+                char ch = (char)(((c - 'a' + shift) % 26) + 'a');
+                result.append(ch);
+            } else {
+                result.append(c); // non-alphabetic characters remain unchanged
+            }
+        }
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter plaintext: ");
+        String text = sc.nextLine();
+        System.out.println("Enter shift key: ");
+        int shift = sc.nextInt();
+
+        String encrypted = encrypt(text, shift);
+        System.out.println("Encrypted text: " + encrypted);
+    }
+}
